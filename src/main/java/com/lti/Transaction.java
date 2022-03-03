a02058b4318c4796c2dbef7877cd589714f2bd15
RiyaAjmeri <24riya99@gmail.com>
2022-03-03 16:32:08 +0530

    Project created

---
+++ src/main/java/com/lti/Transaction.java
@@ -0,0 +1,41 @@
+package com.lti;
+import java.util.LinkedList;
+import java.util.List;
+public class Transaction {
+List<Account> accounts = new LinkedList<Account>();
+	
+
+	public Transaction(List<Account> accounts) {
+		super();
+		this.accounts = accounts;
+	}
+	
+	public Account findAccount(int accNo) {
+		for(Account a: accounts) {
+			if(a.getAccNo() == accNo) {
+				return a;
+			}
+		}
+		return null;
+	}
+	
+	
+	public double withdraw(int accNo, double amt) {
+		Account acc = findAccount(accNo);
+		
+		if (acc.getBalance() > amt) {
+		double temp = acc.getBalance() - amt;
+		acc.setBalance(temp);
+		
+		}
+		return amt;
+	}
+	
+	public double deposit(int accNo, double amt) {
+		Account acc = findAccount(accNo);
+		double temp = acc.getBalance() + amt;
+		acc.setBalance(temp);
+		return temp;
+	}
+
+}
